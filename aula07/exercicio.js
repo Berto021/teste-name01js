293cccd8e6105f8414ae5fea633d62d48b625d50
Humberto Lima <123036971+Berto021@users.noreply.github.com>
2023-03-17 21:52:28 -0300

    Delete aula07 directory

--- aula07/exercicio.js
+++
@@ -1,9 +0,0 @@
-const nome = 'Humberto Ribeiro De Lima';
-const idade = 18;
-const peso = 90;
-const altura = 1.73; //está em metros
-const imc = peso/altura**2;
-let anoNascimento = 2023 -idade - 1;
-console.log(nome,'tem',idade,'anos, pesa', peso +'kg','tem', altura,'metros de altura e seu IMC é de', imc,'e nasceu no ano de',anoNascimento); 
-
-console.log(`O nome dessa pessoa é ${nome} e ele pesa ${peso}kg e tem altura de ${altura}`);//o nome desse ${} é template string, é muito semelhante ao print(f'nananananananananan{variável}nanananananan{variável}) do python
